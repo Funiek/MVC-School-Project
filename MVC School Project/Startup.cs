46c8be1ae3ced3b6e68e2db24c47299d7114dac3
Krzysztof Funkowski <funkowski.krzysztof@gmail.com>
2021-10-17 14:12:13 +0200

    Add project files.

---
+++ MVC School Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_School_Project.Startup))]
+namespace MVC_School_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
